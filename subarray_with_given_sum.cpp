415daf137d0382744e5ab4e98855abbf8bbfbaaa
Aman <aman9verma@hotmail.com>
2017-11-15 23:17:14 +0530

    Modify subarray with given sum.cpp

--- subarray_with_given_sum.cpp
+++ subarray_with_given_sum.cpp
@@ -9,22 +9,20 @@ void calcSubarrays(vector<int> v, int sum) {
             cout << "From index " << start << " to " << i-1 << endl;
             return ;
         }        
-        if (curr_sum > sum && start <= i) {
-            while (curr_sum > sum) {
+        if (curr_sum > sum) {
+            while (curr_sum > sum && start <= i-1) {
                 curr_sum -= v[start++];
                 }
             } 
-        
         if (i < v.size()) 
-	curr_sum += v[i];
+            curr_sum += v[i];
         }
-       
     cout << "No subarray with the given sum" ;        
     }
 
 int main() {
     vector<int> v = {15, 2, 4, 8, 9, 5, 10, 23};
-    int sum = 33;
+    int sum = 28;
     calcSubarrays(v, sum);
     return 0;
     }
