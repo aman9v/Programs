ba4d7bec2ca3e52e33dc09e317bae2bea46d3466
Aman <aman9verma@hotmail.com>
2018-04-13 01:33:54 +0530

    Add constants for entities

---
+++ csv_parsing/constants/constants.js
@@ -0,0 +1,31 @@
+/*jshint esversion:6*/
+
+const iotId = '@iot.id';
+const iotCount = '@iot.count';
+const iotNextLink = '@iot.nextLink';
+const iotSelfLink = '@iot.selfLink';
+const navigationLink = "@iot.navigationLink";
+
+
+
+const entities = [
+  "Things",
+  "Locations",
+  "HistoricalLocations",
+  "Observations",
+  "Sensors",
+  "ObservedProperties",
+  "DataStreams",
+  "FeaturesOfInterest"
+];
+
+const encodingTypes = {
+  UNKNOWN: 'unknown',
+  GEO_JSON: 'application/vnd.geo+json',
+  SENSOR_ML: 'http://www.opengis.net/doc/IS/SensorML/2.0',
+  TEXT_HTML: 'text/html',
+  LOCATION_TYPE: 'http://example.org/location_types#GeoJSON',
+  TYPE_DESCRIPTION: 'http://schema.org/description'
+};
+
+module.exports = { entities, encodingTypes, iotId, iotCount, iotSelfLink, iotNextLink };
